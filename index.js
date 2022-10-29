773415da2d8f24396d60acc55d5c9355d0c7af52
Kwame Shyd <knana6523@gmail.com>
2022-10-29 11:10:45 +0000

    livereload on dev only

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-if(process.NODE_ENV !== "production"){
+if(process.env.NODE_ENV !== "production"){
 
   const livereload = require("livereload");
   const liveReloadServer = livereload.createServer();
